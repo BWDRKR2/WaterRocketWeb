bd15cae394201bc6e8d505b164ef38b68405ce7c
Dan Kozlowski <BWDRKR2@mainoffice.localdomain>
2019-05-03 16:08:21 -0500

    Add Camera HTML

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import requests
 # Initialize the Flask application
 app = Flask(__name__)
 
+camera=html = " "
+
 @app.route('/liftoff/', methods=['GET'])
 def begin():
     r = requests.get('http://10.68.54.45:5000/launch')	
@@ -16,21 +18,22 @@ def begin():
 
 @app.route('/arm/', methods=['GET'])
 def arm():
-    return render_template('main.html', fueling=" ", counter="<h4>Fuel To Launch </h4>" , arm="disabled", unarm="active", fuel="active", launch="disabled" , abort="disabled")
+    return render_template('main.html', camera="visibility: hidden", fueling=" ", counter="<h4>Fuel To Launch </h4>" , arm="disabled", unarm="active", fuel="active", launch="disabled" , abort="disabled")
 
 @app.route('/fueled/<amount>', methods=['GET'])
 def begin_fuel(amount):
     r = requests.get('http://10.68.54.45:5000/begin_fuel/'+ amount)	
     status=r.status_code
-    return render_template('main.html', fueling=" ", counter="<h4>Fueled        </h4>", arm="disabled", unarm="disabled", fuel="disabled", launch="active", abort="active")
+    ##camera_html= "visibility: hidden" 
+    return render_template('main.html', camera=" ", fueling=" ", counter="<h4>Fueled        </h4>", arm="disabled", unarm="disabled", fuel="disabled", launch="active", abort="active")
 
 @app.route('/launch/', methods=['GET'])
 def launch():
-    return render_template('main.html', fueling=" ", counter='<div class="lead", id="clock"></div>', arm="disabled", unarm="disabled", fuel="disabled", launch="disabled", abort="active")
+    return render_template('main.html', camera="visibility: hidden", fueling=" ", counter='<div class="lead", id="clock"></div>', arm="disabled", unarm="disabled", fuel="disabled", launch="disabled", abort="active")
 	
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('main.html', fueling=" ", counter="<h4>Arm To Launch  </h4>", arm="active", unarm="disabled", fuel="disabled", launch="disabled", abort="disabled")
+    return render_template('main.html', camera="visibility: hidden",fueling=" ", counter="<h4>Arm To Launch  </h4>", arm="active", unarm="disabled", fuel="disabled", launch="disabled", abort="disabled")
 
 
 # Run the app :)
